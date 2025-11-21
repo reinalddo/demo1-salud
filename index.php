d2d758e96d23409a11218de141e73a70f9f55aa1
Reinaldo Matheus <reinalddo@gmail.com>
2025-11-21 16:20:00 -0400

    Primer commit: Inicio proyecto medico

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// index.php - Estructura HTML5 básica que muestra 'Hola Mundo' usando PHP
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1><?php echo 'Hola Mundo'; ?></h1>
+</body>
+</html>
