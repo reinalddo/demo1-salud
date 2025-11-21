7feb0a0c9cfa33ea0c1fe3920958d76464cb06e3
Reinaldo Matheus <reinalddo@gmail.com>
2025-11-21 17:15:33 -0400

    Url Amigables

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="index.php">Clínica Salud</a>
+        <a class="navbar-brand fw-bold" href="index">Clínica Salud</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
                 aria-controls="mainNav" aria-expanded="false" aria-label="Alternar navegación">
             <span class="navbar-toggler-icon"></span>
@@ -23,16 +23,16 @@
         <div class="collapse navbar-collapse" id="mainNav">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Inicio</a>
+                    <a class="nav-link" href="index">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="nosotros.php">Nosotros</a>
+                    <a class="nav-link" href="nosotros">Nosotros</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="servicios.php">Servicios</a>
+                    <a class="nav-link" href="servicios">Servicios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contacto.php">Contacto</a>
+                    <a class="nav-link" href="contacto">Contacto</a>
                 </li>
             </ul>
         </div>
