9a4e3442035d51f0154335dec1d3605bfb5e8305
Reinaldo Matheus <reinalddo@gmail.com>
2025-11-21 16:35:27 -0400

    Probando hostinger

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Hola Mundo</title>
+    <title>Hola Mundo!</title>
 </head>
 <body>
     <h1><?php echo 'Hola Mundo'; ?></h1>
